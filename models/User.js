df102c233047e30c22b8ab55f539c6a98d57b4a9
pabhinav1081 <abhinavpandey2405@gmail.com>
2025-12-25 17:32:06 +0530

    done everthing

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  role: { enum: ['user', 'admin'], default: 'user' },
   totalPoints: { type: Number, default: 0 }
 }, { timestamps: true });
 
